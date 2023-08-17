54c8eb3284d22ebd228b15ac3dacc42b703dc3dd
Ashutosh Mohanty <mohantyashu.07@gmail.com>
2023-08-17 16:48:15 +0530

    clean up

--- src/main/java/com/ibm/app/SpringProductCacheApplication.java
+++ src/main/java/com/ibm/app/SpringProductCacheApplication.java
@@ -12,34 +12,4 @@ public class SpringProductCacheApplication {
 		SpringApplication.run(SpringProductCacheApplication.class, args);
 	}
 
-	/*
-	 * @Bean(name = "RedisCacheManager") public CacheManager getManager() {
-	 * JdkSerializationRedisSerializer contextAwareRedisSerializer = new
-	 * JdkSerializationRedisSerializer( getClass().getClassLoader());
-	 * 
-	 * RedisCacheConfiguration redisCacheConfiguration =
-	 * RedisCacheConfiguration.defaultCacheConfig() .disableCachingNullValues() //
-	 * .entryTtl(Duration.ofHours(12) .serializeValuesWith(
-	 * RedisSerializationContext.SerializationPair.fromSerializer(
-	 * contextAwareRedisSerializer));
-	 * 
-	 * return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(
-	 * redisConnectionFactory()) .cacheDefaults(redisCacheConfiguration).build();
-	 * 
-	 * }
-	 * 
-	 * @Bean public RedisConnectionFactory redisConnectionFactory() {
-	 * JedisConnectionFactory factory = new JedisConnectionFactory();
-	 * factory.setHostName("localhost"); // Replace with your Redis server host
-	 * factory.setPort(6379); // Replace with your Redis server port return factory;
-	 * }
-	 * 
-	 * @Bean public RedisTemplate<String, Object> redisTemplate() { final
-	 * RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
-	 * template.setKeySerializer(new StringRedisSerializer());
-	 * template.setHashKeySerializer(new StringRedisSerializer());
-	 * template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-	 * template.setConnectionFactory(redisConnectionFactory()); return template; }
-	 */
-
 }
